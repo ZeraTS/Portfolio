7d1c271762d6710287cc4a5736231ae8af467453
Zera <51274450+ZeraTS@users.noreply.github.com>
2024-03-05 14:41:12 -0500

    commit
    sss

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -28,7 +28,15 @@ module.exports = {
           },
         },
       },
-    },
+      shimmer: {
+        from: {
+          backgroundPosition: "0.0",
+        },
+        to: {
+          backgroundPosition: "-200% 0",
+        },
+        }
+    }
   },
   plugins: [
     addVariablesForColors,
