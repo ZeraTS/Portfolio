5adf4231aa896736169de8e1c178d814a7635ef5
Zera <51274450+ZeraTS@users.noreply.github.com>
2024-04-10 16:01:05 -0400

    Remove Spotify API integration

--- dist/spotify.js
+++
@@ -1,3 +0,0 @@
-import { SpotifyApi } from "@spotify/web-api-ts-sdk";
-const api = SpotifyApi.withClientCredentials("b75ddc1e6be642ab84b70d054b29ff7b", "15b07968c1304f16ad5cf05f186493a4");
-const playingNow = await api.player.getCurrentlyPlayingTrack();
